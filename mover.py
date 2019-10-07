fe53b9a20a1c7b2607bc44d49c6a7ebae4c85f9d
Sebastián Sánchez <ssp013sanchez@gmail.com>
2019-10-07 13:54:07 -0300

    Create mover.py
    Ventanas

---
+++ mover.py
@@ -0,0 +1,8 @@
+import pygame
+from pygame.locals import*
+pygame.init()
+pantalla = pygame.display.set_mode((320,240),0,32)
+
+while True:
+    for eventos in pygame.event.get():
+            exit()
