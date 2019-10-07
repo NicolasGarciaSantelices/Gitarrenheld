175c328622e4f3e305ceece6c375711d94978919
Sebastián Sánchez <ssp013sanchez@gmail.com>
2019-10-06 21:39:12 -0300

    Update ventana.py
    ventana display

--- ventana.py
+++ ventana.py
@@ -1,7 +1,7 @@
 import pygame
 from pygame.locals import *
 pygame.init()
-    pygame.display.flip()pantalla = pygame.display.set_mode((640,480))
+pantalla = pygame.display.set_mode((640,480))
  imagen = pygame.image.load("mario.png")
 while True:
     for eventos in pygame.event.get():
