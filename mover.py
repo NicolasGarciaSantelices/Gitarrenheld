2939712802d38af5a19a9b3690b96d0b45ae8a94
Sebastián Sánchez <ssp013sanchez@gmail.com>
2019-10-07 14:13:02 -0300

    Update mover.py

--- mover.py
+++ mover.py
@@ -1,10 +1,13 @@
 import pygame
-from pygame.locals import*
+from pygame.locals import *
 pygame.init()
-pantalla = pygame.display.set_mode((320,240),0,32)
+pantalla = pygame.display.set_mode((1000,800))
+imagen = pygame.image.load("mario.png")
+imagen = pygame.transform.scale(imagen,(100,100))#Escalamos la imagen en Pygame
 
 while True:
     for eventos in pygame.event.get():
         if eventos.type == pygame.QUIT:
             exit()
-    pulsada = pygame.key.get_pressed()
+    pantalla.blit(imagen,(100,100))
+    pygame.display.update()
