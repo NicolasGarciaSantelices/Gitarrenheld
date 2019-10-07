72cfda6e90b89c8e6cea22ef458c131fe87b5f60
Sebastián Sánchez <ssp013sanchez@gmail.com>
2019-10-06 22:40:53 -0300

    Update test.py
    Final Test entrega 1

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 
 import serial
 import pygame
-arduino = serial.Serial(port='/dev/cu.usbmodem141301', baudrate=9600)
+arduino = serial.Serial(port='/dev/cu.usbmodem146301', baudrate=9600)
 listaInteprete = [0,0,0,0,0,0,0,0]
 pygame.init()
+
 #definir tamaño ventana:
-pantalla = pygame.display.set_mode((640,480))
+pantalla = pygame.display.set_mode((160,32))
+pygame.display.set_caption('Gitarren Held')
 verde1 = pygame.image.load("verde1.png")
 verde2 = pygame.image.load("Verde2.png")
 azul1 = pygame.image.load("Azul1.png")
@@ -17,13 +19,15 @@ amarillo2 = pygame.image.load("Amarillo2.png")
 rojo1 = pygame.image.load("Rojo.png")
 rojo2 = pygame.image.load("Rojo2.png")
 active = pygame.image.load("ON.png")
-
+white = [255, 255, 255]
+pantalla.fill(white)
 while True:
+    pantalla.fill(white)
     line = arduino.readline().decode('utf-8')
     print(line)
     partes = line.split(',')
     for i in range(8):
-        listaInteprete[i]=partes[i]
+        listaInteprete[i]=int(partes[i])
     for eventos in pygame.event.get():
             if eventos.type == pygame.QUIT:
                 exit()
@@ -32,26 +36,28 @@ while True:
     pantalla.blit(rojo1,(32,0))
     pantalla.blit(amarillo1,(64,0))
     pantalla.blit(azul1,(96,0))
-    pantalla.blit(naranja1,(118,0))
+    pantalla.blit(naranja1,(128,0))
     #cambios de estados:
     if(listaInteprete[0]== 1):
         pantalla.blit(verde2,(0,0))
         if(listaInteprete[7]== 1):
             pantalla.blit(active,(0,0))
     if(listaInteprete[1]== 1):
-        pantalla.blit(rojo2,(0,0))
+        pantalla.blit(rojo2,(32,0))
         if(listaInteprete[7]== 1):
-            pantalla.blit(active,(0,0))
+            pantalla.blit(active,(32,0))
     if(listaInteprete[2]== 1):
-        pantalla.blit(amarillo2,(0,0))
+        pantalla.blit(amarillo2,(64,0))
         if(listaInteprete[7]== 1):
-            pantalla.blit(active,(0,0))
+            pantalla.blit(active,(64,0))
     if(listaInteprete[3]== 1):
-        pantalla.blit(azul2,(0,0))
+        pantalla.blit(azul2,(96,0))
         if(listaInteprete[7]== 1):
-            pantalla.blit(active,(0,0))
+            pantalla.blit(active,(96,0))
     if(listaInteprete[4]== 1):
-        pantalla.blit(naranja2,(0,0))
+        pantalla.blit(naranja2,(128,0))
         if(listaInteprete[7]== 1):
-            pantalla.blit(active,(0,0))
+            pantalla.blit(active,(128,0))
+    
+    
     pygame.display.update()
