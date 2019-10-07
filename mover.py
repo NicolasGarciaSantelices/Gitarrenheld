88162027e150207cdf05bb1922da218520ff9e93
Sebastián Sánchez <ssp013sanchez@gmail.com>
2019-10-07 13:54:47 -0300

    Update mover.py
    Mover!!

--- mover.py
+++ mover.py
@@ -5,4 +5,6 @@ pantalla = pygame.display.set_mode((320,240),0,32)
 
 while True:
     for eventos in pygame.event.get():
+        if eventos.type == pygame.QUIT:
             exit()
+    pulsada = pygame.key.get_pressed()
